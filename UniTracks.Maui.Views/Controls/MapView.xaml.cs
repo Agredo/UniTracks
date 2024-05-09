5c45897db0cfdb14bc709f2b6a571eeb087ab743
Agredo <christian@agredoapplication.com>
2024-05-10 00:15:54 +0200

    The most significant changes in the code include the replacement of the `MapView` control with `MapControl` in `MapView.xaml`, the modification of the `LocationChaged` method in `MapView.xaml.cs`, and the addition of a new method `SetPinCallout` in `MapView.xaml.cs`.
    1. The `MapView` control in `MapView.xaml` has been replaced with `MapControl` and the name of the control has been changed from `Map` to `ControlMapView`. This change is likely to affect how the map is displayed and interacted with in the application.
    
    2. The `Mapsui.Projections` namespace is no longer being used in `MapView.xaml.cs`. This could mean that the map projections are now being handled differently or not at all.
    
    3. The `LocationChaged` method in `MapView.xaml.cs` has been significantly modified. Instead of clearing and adding pins to the `ControlMapView`, it now clears and adds drawables. A new `Polyline` object is created and positions are added to it for each location in `newLocartions`. This `Polyline` is then added to the `ControlMapView`'s drawables. This change suggests a shift from using pins to represent locations to using drawables and polylines.
    
    4. If there are any locations in `newLocartions`, `Pin` objects are created for the start and end locations, with specific properties set, and these are added to the `ControlMapView`'s pins. This indicates that while the general use of pins may have been reduced, they are still used for specific purposes such as marking the start and end locations.
    
    5. A new method `SetPinCallout` has been added to `MapView.xaml.cs`. This method sets various properties on a given `Pin` object's callout, including its arrow height, title font size, color, title font color, title, and whether it can be closed by clicking. This new method provides a way to customize the appearance and behavior of pin callouts.

--- UniTracks.Maui.Views/Controls/MapView.xaml.cs
+++ UniTracks.Maui.Views/Controls/MapView.xaml.cs
@@ -2,7 +2,6 @@ using System.Collections.ObjectModel;
 using Location = UniTracks.Models.Location.Location;
 using Mapsui.UI.Maui;
 using Maui.BindableProperty.Generator.Core;
-using Mapsui.Projections;
 
 namespace UniTracks.Maui.Views.Controls;
 
@@ -19,13 +18,60 @@ public partial class MapView : ContentView
 
     private void LocationChaged(Collection<Location> newLocartions)
     {
-        ControlMapView.Pins.Clear();
+        ControlMapView.Drawables.Clear();
+
+        var line = new Mapsui.UI.Maui.Polyline
+        {
+            StrokeWidth = 3, 
+            StrokeColor = Mapsui.UI.Maui.KnownColor.Red, 
+            IsClickable = true, 
+        };
+
         foreach (var location in newLocartions)
         {
-            var smc = SphericalMercator.FromLonLat(location.Latitude, location.Longitude);
-            Position position = new Position(location.Longitude, location.Latitude);
-            var pin = new Pin(ControlMapView) { Label = "Test", Position = position };
-            ControlMapView.Pins.Add(pin);
+            var position = new Position(location.Longitude, location.Latitude);
+            line.Positions.Add(position);
         }
+
+        ControlMapView.Drawables.Add(line);
+
+        //Add Start and End Pin
+        if (newLocartions.Count > 0)
+        {
+            var startPin = new Mapsui.UI.Maui.Pin(ControlMapView)
+            {
+                Label = "Start",
+                Type = PinType.Svg,
+                Position = new Position(newLocartions[0].Longitude, newLocartions[0].Latitude),
+                Scale = 0.5F,
+                Color = Colors.Green,
+            };
+
+            var endPin = new Mapsui.UI.Maui.Pin(ControlMapView)
+            {
+                Label = "End",
+                Type = PinType.Svg,
+                Position = new Position(newLocartions[^1].Longitude, newLocartions[^1].Latitude),
+                Scale = 0.5F,
+                Color = Colors.Red
+            };
+
+            SetPinCallout(startPin, "Start", 10, 14, Colors.Black, Colors.Black);
+            SetPinCallout(endPin, "Finish", 10, 14, Colors.Black, Colors.Black);
+
+            ControlMapView.Pins.Add(startPin);
+            ControlMapView.Pins.Add(endPin);
+        }
+
+    }
+    private void SetPinCallout(Mapsui.UI.Maui.Pin pin, string title, int arrowHeight, int titleFontSize, Color color, Color titleFontColor)
+    {
+        pin.ShowCallout();
+        pin.Callout.ArrowHeight = arrowHeight;
+        pin.Callout.TitleFontSize = titleFontSize;
+        pin.Callout.Color = color;
+        pin.Callout.TitleFontColor = titleFontColor;
+        pin.Callout.Title = title;
+        pin.Callout.IsClosableByClick = false;
     }
 }
